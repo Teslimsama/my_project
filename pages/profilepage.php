a8309ba1e876ec25e892a487fea99ab8496f21e6
Teslimsama <107998616+Teslimsama@users.noreply.github.com>
2022-08-12 02:26:32 +0100

    update stuff

--- pages/profilepage.php
+++ pages/profilepage.php
@@ -76,7 +76,7 @@
             <span class="nav-link-text ms-1">Profile</span>
           </a>
         </li>
-        <li class="nav-item">
+         <li class="nav-item">
           <a class="nav-link text-white " href="./logout.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">assignment</i>

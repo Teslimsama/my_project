84596ee0df96627d5c5073fce0a1b9d244fbb27b
Teslimsama <107998616+Teslimsama@users.noreply.github.com>
2022-08-12 01:05:32 +0100

    added logout files and option

--- admin/dashboard_admin.php
+++ admin/dashboard_admin.php
@@ -84,22 +84,11 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white " href="./Signup.php">
+          <a class="nav-link text-white " href="./logout.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">assignment</i>
             </div>
-            <span class="nav-link-text ms-1">Sign Up</span>
-          </a>
-        </li>
-        <li class="nav-item mt-3">
-          <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Coming Soon...</h6>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white " href="./assignments.php">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">assignment</i>
-            </div>
-            <span class="nav-link-text ms-1">Assignment</span>
+            <span class="nav-link-text ms-1">Log Out</span>
           </a>
         </li>
       </ul>
--- pages/content.php
+++ pages/content.php
@@ -77,19 +77,11 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white " href="./Signin.php">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">login</i>
-            </div>
-            <span class="nav-link-text ms-1">Sign In</span>
-          </a>
-        </li>
-        <li class="nav-item">
           <a class="nav-link text-white " href="./logout.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">assignment</i>
             </div>
-            <span class="nav-link-text ms-1">Sign Up</span>
+            <span class="nav-link-text ms-1">Log Out</span>
           </a>
         </li>
         <li class="nav-item mt-3">

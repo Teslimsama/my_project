418a339184cb848ec110f876960c81c505e84c11
Teslimsama <107998616+Teslimsama@users.noreply.github.com>
2022-09-07 07:33:32 +0100

    changed everything

---
+++ Signin.php
@@ -0,0 +1,150 @@
+<?php
+include('config/alert.message.php');
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In || Unibooks</title>
+    <link rel="apple-touch-icon" sizes="76x76" href="Images/apple-touch-icon.png">
+  <link rel="shortcut icon" type="image/png" href="Images/android-chrome-512x512.png">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/signin.css">
+    <link rel="stylesheet" href="assets/css/material-dashboard.css">
+    <script src="https://kit.fontawesome.com/e9de02addb.js" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+  <div class="container z-index-sticky top-0 mb-5  ">
+    <div class="row">
+      <div class=" ">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg blur border-radius-xl top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4 ">
+          <div class="container-fluid ps-2 pe-0">
+            <a href="Images/unibooks copy.png"> <img class="me-3 " src="Images/unibooks copy.png" alt="" width="50"></a>
+            <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="./about_us" target="_blank">
+             <h4> Unibooks</h4>
+            </a>
+            <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon mt-2 " >
+                <span class="navbar-toggler-bar bar1 "></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+              </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navigation">
+              <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                  <a class="nav-link title d-flex align-items-center me-2 active" aria-current="page" href="./about_us">
+                    <i class="fa-solid fa-table-layout opacity-6 text-dark me-1"></i>
+                    About Us
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link me-2 title" href="./social">
+                    <i class="fa-solid fa-hashtag opacity-6 text-dark me-1"></i>
+                    Social Media
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link me-2 title" href="./Signup">
+                    <i class="fas fa-user-plus opacity-6 text-dark me-1"></i>
+                    Sign Up
+                  </a>
+                </li>
+               
+              </ul>
+
+            </div>
+          </div>
+        </nav>
+        <!-- End Navbar -->
+
+      <main class="container-fluid text-center mt-5 col-lg-12">
+         
+    
+    
+        <div class="form-signin bg-light rounded">
+            
+          
+       <form action="app/signin.app.php" method="POST">
+        <a href="Images/unibooks copy.png"> <img class="me-3 " src="Images/unibooks copy.png" alt="" width="200" height="150"></a>
+         <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+     
+         <?php echo ErrorMessage(); echo SuccessMessage();?>
+         <div class="form-floating">
+         
+           <input type="email" class="form-control  <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php if(isset($_COOKIE["email"])) { echo $_COOKIE["email"]; } ?> " id="floatingInput" name="email" placeholder="Email">
+           <span class="invalid-feedback"><?php echo $username_err; ?></span>
+
+           <label for="floatingInput">Email address</label>
+         </div>
+         <div class="form-floating pass mt-3">
+           <input type="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" id="floatingPassword" name="password" value="<?php if(isset($_COOKIE["password"])) { echo $_COOKIE["password"]; } ?>" placeholder="Password">
+           <span class="invalid-feedback"><?php echo $password_err; ?></span>
+           <span onclick="togglePass()"><i class="fa fa-eye-slash eye"></i></span>
+           <label for="floatingPassword">Password</label>
+         </div>
+         
+         
+     
+         <div class="checkbox mb-3">
+          <div class="form-check form-switch d-flex align-items-center mb-3">
+            <input class="form-check-input" type="checkbox" id="rememberMe" checked>
+            <label class="form-check-label mb-0 ms-3" name="remember">Remember me</label>
+          </div>
+         </div>
+         <button class="w-100 btn btn-lg btn-primary" name="login" type="submit">Sign in</button>
+         <p class="mt-4 text-sm text-center">
+          Don't have an account?
+          <a href="./Signup" class="text-primary text-gradient font-weight-bold">Sign up</a>
+         
+          <p class="text-center text-light mt-3"> 
+         <p class="mt-4 text-sm text-center">
+          Forgotten your 
+          <a href="./forgotten_password" class="text-primary text-gradient font-weight-bold">Password ?</a>
+         
+          <p class="text-center text-light mt-3"> 
+         
+       </form>
+      </div>  
+      <!-- footer  -->
+      <div class="container-fluid bg- mt-5 ">
+          <footer class="py-3 my-4 ">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3 ">
+            <li class="nav-item"><a href="about_us" class="nav-link px-2 text-light">Home</a></li>
+              <li class="nav-item"><a href="#" class="nav-link px-2 text-light">More Website</a></li>
+              <li class="nav-item"><a href="donate" class="nav-link px-2 text-light">Donate</a></li>
+              <li class="nav-item"><a href="faq" class="nav-link px-2 text-light">FAQs</a></li>
+              <li class="nav-item"><a href="about_us" class="nav-link px-2 text-light">About Us</a></li>
+            </ul>
+            <p class="text-center text-light">&copy; 
+              <script>
+              document.write(new Date().getFullYear())
+            </script> Testech, Ltd</p>
+            
+          </footer>
+            </div>
+      <!-- footer  -->
+      </main>
+      <script> function togglePass() {
+          var x = document.getElementById("floatingPassword");
+          if (x.type === "password") {
+            x.type = "text";
+          } else {
+            x.type = "password";
+          }
+          }
+          </script>
+      
+    
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/material-dashboard.js"></script>
+</body>
+</html>
---
+++ success.php
@@ -0,0 +1,209 @@
+<?php
+if ($_GET['status'] !== "success") {
+include_once 'config/database.php';
+header("location:javascript://history.go(-1)");
+}?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="assets/img/favicon.png">
+  <title>
+    Thank You || UniBooks
+  </title>
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+  <!-- Nucleo Icons -->
+  <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- Material Icons -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+  <!-- CSS Files -->
+  <link id="pagestyle" href="assets/css/material-dashboard.css?v=3.0.4" rel="stylesheet" />
+  <link id="pagestyle" href="assets/css/faq.css" rel="stylesheet" />
+  <!-- <link rel="stylesheet" href="assets/css/cheatsheet.css"> -->
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9952650109664010"
+     crossorigin="anonymous"></script>
+  
+</head>
+
+<body class="g-sidenav-show  bg-gray-200">
+  
+        
+       
+  <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+    <!-- Navbar -->
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="true">
+      <div class="container-fluid py-1 px-3">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
+            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Thank You</li>
+          </ol>
+          <h6 class="font-weight-bolder mb-0">Thank You</h6>
+        </nav>
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+          <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+          <form action="search" method="GET">
+            <div class="input-group input-group-outline">
+              <label class="form-label">Type here...</label>
+                <input type="text" name="k"  class="form-control">
+                
+              </div>
+            </form>
+          </div>
+          <ul class="navbar-nav  justify-content-end">
+            <li class="nav-item d-flex align-items-center">
+              <a href="./Signin" class="nav-link text-body font-weight-bold px-0">
+                <i class="fa fa-user me-sm-1"></i>
+                <span class="d-sm-inline d-none">Sign In</span>
+              </a>
+            </li>
+            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+              <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
+                <div class="sidenav-toggler-inner">
+                  <i class="sidenav-toggler-line"></i>
+                  <i class="sidenav-toggler-line"></i>
+                  <i class="sidenav-toggler-line"></i>
+                </div>
+              </a>
+            </li>
+
+
+            
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- End Navbar -->
+    <div class="container-fluid py-4">
+      <div class="row min-vh-80">
+        <div class="col-6 mx-auto">
+          <div class="card mt-4">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+              <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+                <h6 class="text-white text-center text-capitalize ps-3">Successful</h6>
+              
+              </div>
+            </div>
+            <div class="card-body">
+             
+              <div class="message">
+                Thank you ,Your Transaction was Successful
+              </div>
+                
+            </div>
+          </div>
+        </div>
+      </div>
+    <div  class="container-fluid py-4">
+         <!-- footer  -->
+         <div class="container-fluid bg- mt-5 ">
+          <footer class="py-3 my-4 ">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+              <li class="nav-item"><a href="about_us" class="nav-link px-2 text-light">Home</a></li>
+              <li class="nav-item"><a href="#" class="nav-link px-2 text-light">More Website</a></li>
+              <li class="nav-item"><a href="donate" class="nav-link px-2 text-light">Donate</a></li>
+              <li class="nav-item"><a href="faq" class="nav-link px-2 text-light">FAQs</a></li>
+              <li class="nav-item"><a href="about_us" class="nav-link px-2 text-light">About Us</a></li>
+            </ul>
+            <p class="text-center text-light">&copy; 
+              <script>
+              document.write(new Date().getFullYear())
+            </script><a href=""> Testech, Ltd</a></p>
+            
+          </footer>
+            </div>
+      <!-- footer  -->
+    </div>
+    </div>
+  </main>
+  <div class="fixed-plugin">
+    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
+      <i class="material-icons py-2">Theme</i>
+    </a>
+    <div class="card shadow-lg">
+      <div class="card-header pb-0 pt-3">
+        <div class="float-start">
+          <h5 class="mt-3 mb-0">Unibook  UI Configurator</h5>
+          <p>See our dashboard options.</p>
+        </div>
+        <div class="float-end mt-4">
+          <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
+            <i class="material-icons">clear</i>
+          </button>
+        </div>
+        <!-- End Toggle Button -->
+      </div>
+      <hr class="horizontal dark my-1">
+      <div class="card-body pt-sm-3 pt-0">
+        <!-- Sidebar Backgrounds -->
+        <div>
+          <h6 class="mb-0">Sidebar Colors</h6>
+        </div>
+        <a href="javascript:void(0)" class="switch-trigger background-color">
+          <div class="badge-colors my-2 text-start">
+            <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
+          </div>
+        </a>
+        <!-- Sidenav Type -->
+        <div class="mt-3">
+          <h6 class="mb-0">Sidenav Type</h6>
+          <p class="text-sm">Choose between 2 different sidenav types.</p>
+        </div>
+        <div class="d-flex">
+          <button class="btn bg-gradient-dark px-3 mb-2 active" data-class="bg-gradient-dark" onclick="sidebarType(this)">Dark</button>
+          <button class="btn bg-gradient-dark px-3 mb-2 ms-2" data-class="bg-transparent" onclick="sidebarType(this)">Transparent</button>
+          <button class="btn bg-gradient-dark px-3 mb-2 ms-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
+        </div>
+        <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
+        <!-- Navbar Fixed -->
+        <div class="mt-3 d-flex">
+          <h6 class="mb-0">Navbar Fixed</h6>
+          <div class="form-check form-switch ps-0 ms-auto my-auto">
+            <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
+          </div>
+        </div>
+        <hr class="horizontal dark my-3">
+        <div class="mt-2 d-flex">
+          <h6 class="mb-0">Light / Dark</h6>
+          <div class="form-check form-switch ps-0 ms-auto my-auto">
+            <input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
+          </div>
+      </div>
+    </div>
+  </div>
+  
+  <!--   Core JS Files   -->
+  <script src="assets/js/core/popper.min.js"></script>
+  <script src="assets/js/core/bootstrap.min.js"></script>
+  <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script src="a"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="assets/js/material-dashboard.min.js?v=3.0.4"></script>
+</body>
+
+</html>

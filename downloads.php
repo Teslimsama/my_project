f5f4a1132cef6cd169e8a29428b079bf5f1f1597
Bolaji Teslim <bolajiteslim05@gmail.com>
2024-07-19 16:13:59 +0100

    made table to be a datatable

--- downloads.php
+++ downloads.php
@@ -1,5 +1,4 @@
-<?php include "session.php";
-?>
+<?php include "session.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,29 +7,28 @@
   <title>
     Downloads || Unibooks
   </title>
-  <!--     Fonts and icons     -->
+  <!-- Fonts and icons -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
   <!-- Nucleo Icons -->
   <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
   <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
-
-
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/e9de02addb.js" crossorigin="anonymous"></script>
-
-
-  <script async src="https://page.phpad2.googlesyndication.com/page.phpad/js/adsbygoogle.js?client=ca-pub-9952650109664010" crossorigin="anonymous"></script>
-
-
-
   <!-- Material Icons -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
   <!-- CSS Files -->
-  <link id="page.phpstyle" href="assets/css/material-dashboard.css?v=3.0.4" rel="stylesheet" />
-  <link rel="stylesheet" href="assets/css/content.css">
+  <link id="pagestyle" href="assets/css/material-dashboard.css?v=3.0.4" rel="stylesheet" />
+  <link id="pagestyle" href="assets/css/faq.css" rel="stylesheet" />
+  <!-- DataTables CSS -->
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap5.min.css" />
+  <!-- DataTables JS -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js"></script>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9952650109664010" crossorigin="anonymous"></script>
 </head>
 
-<body class="g-sidenav-show  bg-gray-200">
+<body class="g-sidenav-show bg-gray-200">
   <?php include 'sidebar.php' ?>
 
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
@@ -50,12 +48,10 @@
               <div class="input-group input-group-outline">
                 <label class="form-label">Type here...</label>
                 <input type="text" name="k" class="form-control">
-
               </div>
             </form>
           </div>
           <?php include "navbar.php" ?>
-
         </div>
       </div>
     </nav>
@@ -63,10 +59,10 @@
 
     <div class="row">
       <div class="col-12">
-        <div class="card dets ">
+        <div class="card dets">
           <div class="card-body px-0 pb-2">
             <div class="table-responsive">
-              <table class="table align-items-center mb-0">
+              <table id="downloads-table" class="table table-striped align-items-center mb-0">
                 <thead>
                   <tr>
                     <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">#</th>
@@ -80,12 +76,8 @@
                     $stmt = $conn->prepare("SELECT * FROM downloads WHERE customerid=? ORDER BY id DESC");
                     $stmt->execute([$student_id]);
                     $n = 1;
-                    while ($patient_rows = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                      $date = $patient_rows['date'];
-
-
-
-
+                    while ($download_rows = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                      $date = $download_rows['date'];
                   ?>
                       <tr>
                         <td>
@@ -97,16 +89,13 @@
                         </td>
                         <td>
                           <div class="avatar-group mt-2">
-                            <a href="javascript:;" class="" data-bs-toggle="tooltip" data-bs-placement="bottom" title="<?php echo  $patient_rows['book_id'];
-                                                                                                                        ?>">
-                              <h6> <?php echo  $patient_rows['book_id'];
-                                    ?></h6>
-
+                            <a href="javascript:;" class="" data-bs-toggle="tooltip" data-bs-placement="bottom" title="<?php echo $download_rows['book_id']; ?>">
+                              <h6><?php echo $download_rows['book_id']; ?></h6>
                             </a>
                           </div>
                         </td>
                         <td class="align-middle text-center text-sm">
-                          <span class="text-xs font-weight-bold"> <?php echo $date; ?> </span>
+                          <span class="text-xs font-weight-bold"><?php echo $date; ?></span>
                         </td>
                       </tr>
                   <?php $n++;
@@ -121,23 +110,17 @@
         </div>
       </div>
     </div>
-    </div>
-    </div>
 
     <?php include "footer.php" ?>
 
-    <!-- footer  -->
-    </div>
-    </div>
   </main>
   <?php include "plugin.php" ?>
 
-  <!--   Core JS Files   -->
+  <!-- Core JS Files -->
   <script src="assets/js/core/popper.min.js"></script>
   <script src="assets/js/core/bootstrap.min.js"></script>
   <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
   <script src="assets/js/plugins/smooth-scrollbar.min.js"></script>
-  <script src="a"></script>
   <script>
     var win = navigator.platform.indexOf('Win') > -1;
     if (win && document.querySelector('#sidenav-scrollbar')) {
@@ -151,6 +134,24 @@
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="assets/js/material-dashboard.min.js?v=3.0.4"></script>
+  <!-- DataTables Initialization -->
+  <script>
+    $(document).ready(function() {
+      $('#downloads-table').DataTable({
+        "stripeClasses": [],
+        "order": [
+          [0, "asc"]
+        ],
+        "language": {
+          "paginate": {
+            "previous": "<span aria-hidden='true'>«</span>",
+            "next": "<span aria-hidden='true'>»</span>"
+          }
+        },
+        "pageLength": 10
+      });
+    });
+  </script>
 </body>
 
 </html>
